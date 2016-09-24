27b8cbc06709ff5ddb4a58a3234361379a4cb50f
AndyWang2015 <andy2472625@gmail.com>
2016-09-24 13:09:24 +0800

    andy

--- src/js/common.js
+++ src/js/common.js
@@ -3,5 +3,26 @@
 		wrp: $('.wrapper')
 	};
 	
-  
+	$('.menubtn').on('click',function(){
+		if($(this).parent().hasClass('on')){
+			showMenu(false);
+		}else{
+			showMenu(true);
+		}
+	});
+
+
+
+
+
+
+
+	//Event
+	function showMenu(_t){
+		if(_t){
+			$('.head_top').addClass('on');
+		}else{
+			$('.head_top').removeClass('on');
+		}
+	}
 })//ready end  
