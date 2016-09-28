bb899393943bee23d8a32dff9d030a4e842000ec
andy <andy2472625@gmail.com>
2016-09-28 22:27:21 +0800

    andy

--- src/js/common.js
+++ src/js/common.js
@@ -3,10 +3,29 @@
 		wrp: $('.wrapper'),
 		loading: $('.loading'),
 		backEndUrl: 'http://benefique-event.medialand.com.tw/xml/',
-		menuTimeout:''
+		menuTimeout:'',
+		agreeTxt:'請同意個人條款',
+		nameTxt:'請輸入真實姓名',
+		phoneTxt:'請輸入聯絡手機',
+		codeTxt:'請輸入驗證碼',
+		emailTxt:'請輸入E-mail',
+		messageTxt:'請輸入留言',
+		FBAppId: '176845812757265',
+		mainurl: 'http://benefique-event.medialand.com.tw/',
+		videolink: 'https://www.youtube.com/watch?v=8bko-b90m_M',
+		exchangeTitle:'已經將您的留言傳送給媽媽了！',
+		exchangeTxt:'送您一份禮物',
+		exchangeTitle_mom:'親愛的媽媽<br>這是女兒給您的溫暖留言：',
+		exchangeTxt_mom:'還有一份禮物'
 	};
 
 	//Common
+	$('.logo').on('click',function(){
+		window.location.href="http://benefique-event.medialand.com.tw/m/";
+	})
+	$('.menu_social .fb_btn').on('click',function(){
+		window.open('https://www.facebook.com/taiwanshiseido/');
+	});
 	$('.menubtn').on('click',function(){
 		if($(this).parent().hasClass('on')){
 			showMenu(false);
@@ -14,10 +33,17 @@
 			showMenu(true);
 		}
 	});
+	window.onorientationchange = readDeviceOrientation;
 	$(window).load(function(){
-		o.loading.fadeOut();
+		
+		readDeviceOrientation();
 		if(o.wrp.hasClass('idx')) initIdx();
 		if(o.wrp.hasClass('ipt')) initIpt();
+		if(o.wrp.hasClass('video')) initVideo();
+		if(o.wrp.hasClass('product')) initProduct();
+		
+		if(o.wrp.hasClass('exchange')) initExchange();
+		else o.loading.fadeOut();
 	});
 	function showMenu(_t){
 		if(_t){
@@ -34,13 +60,247 @@
 			},800);
 		}
 	}
+	function readDeviceOrientation() {                 		
+	    if (Math.abs(window.orientation) === 90) {
+	        // Landscape
+	        $('.turn_straight').fadeIn();
+	    } else {
+	    	// Portrait
+	    	$('.turn_straight').fadeOut();
+	    }
+	}
 
+
+	
+	//product
+	function initExchange(){
+		getExchange();
+
+		$('.go_btn').on('click',function(){
+			if(!$(this).hasClass('on')){
+				showConform_pop(true);
+			}
+		});
+		$('.conform_pop .closebtn').on('click',function(){
+			showConform_pop(false);
+		});
+
+		function sureChange(){
+			var getcode = window.location.href.split('.html?')[1];
+			$.ajax({
+				url: o.backEndUrl + 'api_exchange_confirm.ashx',
+				type: 'POST',
+				dataType: 'json',  
+				data:{
+					qs:getcode
+				},    
+				success: function(data) {
+					if(data.RS=="OK"){
+						$('.go_btn').addClass('on');
+						showConform_pop(false);
+					}else{
+						alert(data.RS);
+					}
+				},error: function(xhr, textStatus, errorThrown) {
+					console.log("error:", xhr, textStatus, errorThrown);
+				}
+			}); 
+		}
+		function showConform_pop(_t){
+			if(_t){
+				$('.conform_pop').fadeIn();
+			}else{
+				$('.conform_pop').fadeOut();
+			}
+		}
+		function getExchange(){
+			var getcode = window.location.href.split('.html?')[1];
+			$.ajax({
+				url: o.backEndUrl + 'api_exchange.ashx',
+				type: 'POST',
+				dataType: 'json',  
+				data:{
+					qs:getcode
+				},    
+				success: function(data) {
+					if(data.RS=="OK"){
+						o.exchangeData = data;
+						console.log(o.exchangeData);
+						afterGetdata();
+						o.loading.fadeOut();
+					}else{
+						o.loading.fadeOut();
+						alert(data.RS);
+					}
+				},error: function(xhr, textStatus, errorThrown) {       
+					o.loading.fadeOut();    
+					console.log("error:", xhr, textStatus, errorThrown);
+				}
+			}); 
+		}
+		function afterGetdata(){
+			if(o.exchangeData.EXCHANGE!=0) $('.go_btn').addClass('on');
+			else{
+				$('.yes_btn').on('click',function(){
+					sureChange();
+				});
+				$('.no_btn').on('click',function(){
+					showConform_pop(false);
+				});
+			}
+			$('.store').html(o.exchangeData.STORE);
+
+			var mom = false;
+
+			if(mom){
+				$('.message .t').html(o.exchangeTitle_mom);
+				$('.message .w').html(o.exchangeData.MESSAGE);
+				$('.type').html(o.exchangeTxt_mom);
+			}else{
+				$('.message .t').html(o.exchangeTitle);
+				$('.message .w').html(o.exchangeData.MESSAGE);
+				$('.type').html(o.exchangeTxt);
+			}
+
+		}
+	}
+	function initProduct(){
+		$('.pd_box').addClass('on');
+	}
+	//video
+	function initVideo(){
+		FB.init({
+	        appId      : o.FBAppId,
+	        channelUrl : o.mainurl,
+	        status     : true,
+	        xfbml      : true,
+	        cookie     : true
+	    });
+
+		$('.about_btn').on('click',function(){
+			showAboutpop(true);
+		});
+		$('.about_pop .closebtn').on('click',function(){
+			showAboutpop(false);
+		});
+		$('.video_success_pop .closebtn').on('click',function(){
+			showSuccessVideopop(false);
+		});
+		$('.video_data_pop .sendbtn a').on('click',function(){
+			if(!$(this).hasClass('on')) video_send_data();
+		});
+		$('.video_data_pop .agree_box .checkbox').on('click',function(){
+			if($(this).hasClass('on')) $(this).removeClass('on');
+			else $(this).addClass('on');
+		});
+		$('.video_data_pop .closebtn').on('click',function(){
+			showVideoPop(false);
+		});
+		$('.sharebtn a').on('click',function(){
+			shareVideo();
+			showVideoPop(true);
+		});
+
+		function showSuccessVideopop(_t){
+			if(_t){
+				$('.video_success_pop').fadeIn();
+			}else{
+				$('.video_success_pop').fadeOut();
+			}
+		}
+		function clearVideopop(){
+			var _data_box = $('.video_data_box');
+			_data_box.find('.name').val('');
+			_data_box.find('.phone').val('');
+			_data_box.find('.email').val('');
+			_data_box.find('.code').val('');
+		}
+		function video_send_data(){
+			var _o = $('.video_data_pop .sendbtn a');
+			_o.addClass('on');
+
+			var _data_box = $('.video_data_box');
+			var _data = {
+				name:_data_box.find('.name').val(),
+				mob:_data_box.find('.phone').val(),
+				email:_data_box.find('.email').val(),
+				ccc:_data_box.find('.code').val(),
+			};
+			console.log(_data);
+			if(_data.name == "" || _data.name == undefined){alert(o.nameTxt);_o.removeClass('on');return;}
+			if(_data.mob == "" || _data.mob == undefined){alert(o.phoneTxt);_o.removeClass('on');return;}
+			if(_data.email == "" || _data.email == undefined){alert(o.emailTxt);_o.removeClass('on');return;}
+			if(_data.ccc == "" || _data.ccc == undefined){alert(o.codeTxt);_o.removeClass('on');return;}
+			if(!$('.agree_box .checkbox').hasClass('on')){alert(o.agreeTxt);_o.removeClass('on');return;}
+
+			$.ajax({
+				url: o.backEndUrl + 'api_info.ashx',
+				type: 'POST',
+				dataType: 'json',  
+				data:_data,    
+				success: function(data) {
+					if(data.RS=="OK"){
+						clearVideopop();
+						showVideoPop(false);
+						showSuccessVideopop(true);
+					}else{
+						_o.removeClass('on');
+						alert(data.RS);
+					}
+				},error: function(xhr, textStatus, errorThrown) {       
+					_o.removeClass('on');      
+					console.log("error:", xhr, textStatus, errorThrown);
+				}
+			}); 
+		}
+		function showVideoPop(_t){
+			if(_t){
+				$('.video_data_pop').fadeIn();
+			}else{
+				$('.video_data_pop').fadeOut();
+			}
+		}
+		function shareVideo(){
+			FB.ui({             
+				method: 'feed',
+				// name: o.sharetitle,
+				// description: o.sharedes,
+				display:"popup",
+				link: o.videolink
+			}, function(response) {
+
+			});    
+		}
+		function showAboutpop(_t){
+			if(_t){
+				$('.about_pop').fadeIn();
+			}else{
+				$('.about_pop').fadeOut();
+			}
+		}
+	}
 
 	//Input
 	function initIpt(){
 		changeCode();
 		getData();
 
+		$('.resend_pop .sendbtn a').click(function(){
+			if(!$(this).hasClass('on')){
+				$(this).addClass('on');
+				resendData();
+			}
+		});
+		$('.success_pop .sendbtn a').click(function(){
+			showResendpop(true);
+			showSuccesspop(false);
+		});
+		$('.stage_input .sendbtn a').click(function(){
+			if(!$(this).hasClass('on')){
+				$(this).addClass('on');
+				saveData();
+			}
+		});
 		$('.respend').click(function(){
 			showResendpop(true);
 		});
@@ -60,6 +320,48 @@
 			else $(this).removeClass('on');
 		});
 
+
+		function clearResendpop(){
+			var _resend_data_box = $('.resend_data_box');
+			_resend_data_box.find('.phone').val('');
+			_resend_data_box.find('.email').val('');
+			_resend_data_box.find('.code').val('');
+		}
+		function resendData(){
+			var _o = $('.resend_pop .sendbtn a');
+			_o.addClass('on');
+
+			var _resend_data_box = $('.resend_data_box');
+			var _data = {
+				mob:_resend_data_box.find('.phone').val(),
+				email:_resend_data_box.find('.email').val(),
+				ccc:_resend_data_box.find('.code').val()
+			};
+			console.log(_data);
+			if(_data.mob == "" || _data.mob == undefined){alert(o.phoneTxt);_o.removeClass('on');return;}
+			if(_data.email == "" || _data.email == undefined){alert(o.emailTxt);_o.removeClass('on');return;}
+			if(_data.ccc == "" || _data.ccc == undefined){alert(o.codeTxt);_o.removeClass('on');return;}
+
+			$.ajax({
+				url: o.backEndUrl + 'api_resend.ashx',
+				type: 'POST',
+				dataType: 'json',  
+				data:_data,    
+				success: function(data) {
+					if(data.RS=="OK"){
+						alert('補發成功');
+						clearResendpop();
+						showResendpop(false);
+					}else{
+						_o.removeClass('on');
+						alert(data.RS);
+					}
+				},error: function(xhr, textStatus, errorThrown) {       
+					_o.removeClass('on');      
+					console.log("error:", xhr, textStatus, errorThrown);
+				}
+			}); 
+		}
 		function showResendpop(_t){
 			if(_t){
 				$('.resend_pop').fadeIn();
@@ -70,6 +372,8 @@
 		function showSuccesspop(_t){
 			if(_t){
 				$('.success_pop').fadeIn();
+				$('.stage_input .sendbtn a').removeClass('on');
+				clearSavedata();
 			}else{
 				$('.success_pop').fadeOut();
 			}
@@ -84,15 +388,70 @@
 				type: 'POST',
 				dataType: 'json',      
 				success: function(data) {
-				if(data.RS=="OK"){
-					o.StoreData = data.DATA;
-					console.log(o.StoreData);
-					$('.data_box').each(data_boxFC);
-				}else alert(data.RS);
+					if(data.RS=="OK"){
+						o.StoreData = data.DATA;
+						// console.log(o.StoreData);
+						$('.data_box').each(data_boxFC);
+					}else alert(data.RS);
 				},error: function(xhr, textStatus, errorThrown) {             
 					console.log("error:", xhr, textStatus, errorThrown);
 				}
 			}); 
+		}
+		function clearSavedata(){
+			var _data_box = $('.your_data_box');
+			var _data_box2 = $('.mom_data_box');
+			_data_box.find('.name').val('');
+			_data_box.find('.phone').val('');
+			_data_box2.find('.name').val('');
+			_data_box2.find('.phone').val('');
+			$('.message_box .message').val('');
+			$('.stage_input .code_box .code').val('');
+		}
+		function saveData(){
+			var _o = $('.stage_input .sendbtn a');
+			_o.addClass('on');
+
+			var _data_box = $('.your_data_box');
+			var _data_box2 = $('.mom_data_box');
+			var _data = {
+				name:_data_box.find('.name').val(),
+				mob:_data_box.find('.phone').val(),
+				storeid:_data_box.find('.store').find('option:selected').attr('storeid'),
+				city:_data_box.find('.city').find('option:selected').val(),
+				name2:_data_box2.find('.name').val(),
+				mob2:_data_box2.find('.phone').val(),
+				storeid2:_data_box2.find('.store').find('option:selected').attr('storeid'),
+				city2:_data_box2.find('.city').find('option:selected').val(),
+				message:$('.message_box .message').val(),
+				ccc:$('.stage_input .code_box .code').val()
+			};
+			console.log(_data);
+			if(_data.name == "" || _data.name == undefined){alert(o.nameTxt);_o.removeClass('on');return;}
+			if(_data.mob == "" || _data.mob == undefined){alert(o.phoneTxt);_o.removeClass('on');return;}
+			if(_data.name2 == "" || _data.name2 == undefined){alert(o.nameTxt);_o.removeClass('on');return;}
+			if(_data.mob2 == "" || _data.mob2 == undefined){alert(o.phoneTxt);_o.removeClass('on');return;}
+			if(_data.ccc == "" || _data.ccc == undefined){alert(o.codeTxt);_o.removeClass('on');return;}
+			if(_data.message == "" || _data.message == undefined){alert(o.messageTxt);_o.removeClass('on');return;}
+			if(!$('.agree_box .checkbox').hasClass('on')){alert(o.agreeTxt);_o.removeClass('on');return;}
+
+			$.ajax({
+				url: o.backEndUrl + 'api_save.ashx',
+				type: 'POST',
+				dataType: 'json',  
+				data:_data,    
+				success: function(data) {
+					if(data.RS=="OK"){
+						showSuccesspop(true);
+					}else{
+						_o.removeClass('on');
+						alert(data.RS);
+					}
+				},error: function(xhr, textStatus, errorThrown) {  
+					_o.removeClass('on');           
+					console.log("error:", xhr, textStatus, errorThrown);
+				}
+			});
 		}
 		function data_boxFC(){
 			var _this = $(this),
@@ -137,7 +496,7 @@
 			}
 			function areaChange(){
 				_store.html('');
-				for(i in o.StoreData[nowCity].areas[nowArea].STORES) _store.append('<option value="'+ o.StoreData[nowCity].areas[nowArea].STORES[i].STORE +'">'+  o.StoreData[nowCity].areas[nowArea].STORES[i].STORE +'</option>');
+				for(i in o.StoreData[nowCity].areas[nowArea].STORES) _store.append('<option storeid="'+ o.StoreData[nowCity].areas[nowArea].STORES[i].ID +'" value="'+ o.StoreData[nowCity].areas[nowArea].STORES[i].STORE +'">'+  o.StoreData[nowCity].areas[nowArea].STORES[i].STORE +'</option>');
 				nowStore = 0;
 				storeChange();
 			}
